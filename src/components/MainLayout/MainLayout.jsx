f69240bfe042d569091117f4a25e63b5cf274279
noornabi07 <noornabiprogram07@gmail.com>
2023-06-07 09:08:37 +0600

    my project martial art start first setup

---
+++ src/components/MainLayout/MainLayout.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainLayout = () => {
+    return (
+        <div>
+            <h2>This is main page..</h2>
+        </div>
+    );
+};
+
+export default MainLayout;
