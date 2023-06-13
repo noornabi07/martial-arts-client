5d16192baeaf1165f4426de84fb33f4c1b74a7b5
noornabi07 <noornabiprogram07@gmail.com>
2023-06-13 19:12:05 +0600

    some make responsive in navbar

--- src/components/Shared/Navbar/Navbar.jsx
+++ src/components/Shared/Navbar/Navbar.jsx
@@ -47,20 +47,19 @@ const Navbar = () => {
 
             </>
         }
-
     </>
 
 // first div using classes (max-w-screen-xl mx-auto )
 
     return (
         <>
-            <div className="navbar bg-opacity-80 lg:mx-auto  bg-black text-white">
+            <div className="navbar bg-opacity-80 max-w-screen-xl  bg-black text-white">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
-                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <ul tabIndex={0} className="menu menu-compact z-20 dropdown-content mt-3 p-2 shadow bg-red-500 rounded-box w-52">
                             {navOptions}
                         </ul>
                     </div>
